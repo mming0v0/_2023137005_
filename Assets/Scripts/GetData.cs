1f7929b021efe16b6540562af534b6aee0d61adc
mming0v0 <minji10221004@gmail.com>
2023-10-05 12:04:26 +0900

    Feat: "Spawer 까지 진행"

---
+++ Assets/Scripts/GetData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetData : MonoBehaviour
+{
+
+    //public Entity_GameDB entity_gamedb;
+    //// start is called before the first frame update
+    //void Start()
+    //{
+    //    foreach (Entity_GameDB, Param param in entity_gamedb.sheets[0].list)
+    //    {
+    //        Debug.Log(param, index + " - " + param.characterName + " - " + param.hp + " - " + param.mp);
+    //    }
+    //}
+
+    //// update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
